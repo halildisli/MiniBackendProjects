8db789e68e9e6ca674ca386dd265abadfc94085d
Halil Ibrahim Disli <halil.disli@outlook.com>
2023-06-23 18:23:15 +0300

    JwtProject added repo.

---
+++ JwtProject/JwtProject/Context/JwtDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtProject.Context
+{
+    public class JwtDbContext:IdentityDbContext
+    {
+        public JwtDbContext(DbContextOptions<JwtDbContext> options):base(options)
+        {
+            
+        }
+    }
+}
---
+++ JwtProject/JwtProject/Extensions/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using JwtProject.Context;
+
+namespace JwtProject.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<JwtDbContext>(options=>options.UseSqlServer)
+        }
+    }
+}
